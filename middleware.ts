3805a7b3db75388c92772c341c3a15d54e7e0b18
Suzan <jaipurisuzan23@gmail.com>
2024-08-06 18:30:13 +0530

    added changes

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,30 @@
-// Resource: https://clerk.com/docs/nextjs/middleware#auth-middleware
-// Copy the middleware code as it is from the above resource
+// // Resource: https://clerk.com/docs/nextjs/middleware#auth-middleware
+// // Copy the middleware code as it is from the above resource
 
-import { authMiddleware } from "@clerk/nextjs";
+// import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default authMiddleware({
-  // An array of public routes that don't require authentication.
-  publicRoutes: ["/", "/api/webhook/clerk"],
 
-  // An array of routes to be ignored by the authentication middleware.
-  ignoredRoutes: ["/api/webhook/clerk"],
-});
+
+// export default clerkMiddleware({
+//   // An array of public routes that don't require authentication.
+//   publicRoutes: ["/", "/api/webhook/clerk"],
+
+//   // An array of routes to be ignored by the authentication middleware.
+//   ignoredRoutes: ["/api/webhook/clerk"],
+// });
+
+// export const config = {
+//   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
+import { clerkMiddleware } from '@clerk/nextjs/server';
+
+export default clerkMiddleware()
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
 };
